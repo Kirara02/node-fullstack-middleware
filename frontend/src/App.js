6b8e07287877936587db94606ba687cedebef351
Kirara02 <fathul.sirnaraja92@gmail.com>
2022-12-18 20:35:46 +0700

    FINISH

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import { BrowserRouter, Routes , Route } from 'react-router-dom';
+import Dashboard from './components/Dashboard';
+import Login from "./components/Login";
+import Navbar from './components/Navbar';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+        <Route path='/dashboard' element={
+          <div>
+            <Navbar/>
+            <Dashboard/>
+          </div>
+        }/>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Login.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [msg, setMsg] = useState("");
+  const navigate = useNavigate();
+
+  const Login = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:5000/login", {
+        email: email,
+        password: password,
+      });
+      navigate("/dashboard");
+    } catch (error) {
+      if (error.response) {
+        setMsg(error.response.data.msg);
+      }
+    }
+  };
+
+  return (
+    <section className="hero is-success is-fullheight">
+      <div className="hero-body">
+        <div className="container">
+          <div className="columns is-centered">
+            <div className="column is-4-desktop">
+              <form className="box" onSubmit={Login}>
+                <p className="has-text-centered">{msg}</p>
+                <div className="field mt-5">
+                  <label className="label">Email</label>
+                  <div className="controls">
+                    <input
+                      type="text"
+                      className="input"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      placeholder="Email"
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <label className="label">Password</label>
+                  <div className="controls">
+                    <input
+                      type="password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      className="input"
+                      placeholder="*******"
+                    />
+                  </div>
+                </div>
+                <div className="field mt-5">
+                  <button className="button is-success is-fullwidth">
+                    Login
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
